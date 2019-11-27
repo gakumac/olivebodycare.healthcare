00f0a529ba32b3fab50fa13a4a269842bcd29f55
gakumac <gaku.machida@gmail.com>
2019-11-27 12:07:58 +0900

    ナビゲーションバー・メニューを修正

--- assets/configs/menus.js
+++ assets/configs/menus.js
@@ -4,6 +4,11 @@ export default {
       action: 'home',
       title: 'Home',
       to: '/'
+    },
+    {
+      action: 'face',
+      title: '患者様の声',
+      to: '/voice'
     }
   ],
   items: [
@@ -14,38 +19,31 @@ export default {
       items: [
         {
           title: '施術メニュー・料金',
-          to: '/menu',
-          action: ''
+          to: '/menu'
         },
         {
           title: '整体について',
-          to: '/menu/seitai',
-          action: ''
+          to: '/menu/seitai'
         },
         {
           title: '骨盤矯正について',
-          to: '/menu/pelvis-correction',
-          action: ''
+          to: '/menu/pelvis-correction'
         },
         {
           title: 'マッサージについて',
-          to: '/menu/massage',
-          action: ''
+          to: '/menu/massage'
         },
         {
           title: '鍼灸について',
-          to: '/menu/shinkyu',
-          action: ''
+          to: '/menu/shinkyu'
         },
         {
           title: '不妊治療',
-          to: '/menu/fertility-treatment',
-          action: ''
+          to: '/menu/fertility-treatment'
         },
         {
           title: '交通事故治療',
-          to: '/menu/traffic-accident',
-          action: ''
+          to: '/menu/traffic-accident'
         }
       ]
     },
@@ -56,49 +54,52 @@ export default {
       items: [
         {
           title: '当院について',
-          to: '/about',
-          action: ''
+          to: '/about'
         },
         {
           title: '総院長からの挨拶',
-          to: '/about/greeting',
-          action: ''
+          to: '/about/greeting'
         },
         {
           title: 'スタッフについて',
-          to: '/about/staff',
-          action: ''
+          to: '/about/staff'
         },
         {
           title: 'Q&A-良くある質問-',
-          to: '/about/qa',
-          action: ''
+          to: '/about/qa'
         },
         {
           title: '入店から施術までの流れ',
-          to: '/about/beginner',
-          action: ''
+          to: '/about/beginner'
         },
         {
           title: 'お問合せ・メール予約',
-          to: '/about/contact',
-          action: ''
+          to: '/about/contact'
         }
       ]
-    }
-  ],
-  itemsVoice: [
-    {
-      action: 'face',
-      title: '患者様の声',
-      to: '/voice'
-    }
-  ],
-  itemsSalon: [
+    },
     {
       action: 'place',
-      title: '治療院一覧',
-      to: '/salon'
+      title: '治療院の紹介',
+      active: true,
+      items: [
+        {
+          title: '治療院一覧',
+          to: '/salon'
+        },
+        {
+          title: 'たまプラーザ本店',
+          to: '/salon/tamaplaza'
+        },
+        {
+          title: '横浜青葉台店',
+          to: '/salon/aobadai'
+        },
+        {
+          title: '札幌円山店',
+          to: '/salon/maruyama'
+        }
+      ]
     }
   ]
 }
