a4898027bc72a8976ebacc76c0189f84f4ebaf26
gakumac <gaku.machida@gmail.com>
2020-02-05 11:30:49 +0900

    青葉台店のキャンペーン情報を追加

--- pages/about/medical-questionnaire.vue
+++ pages/about/medical-questionnaire.vue
@@ -25,7 +25,7 @@ export default {
         description:
           '整体・骨盤矯正の女性専門の治療院オリーヴボディケア。当院では患者様にご来院して頂いた際、施術のお時間をなるべく多くおとりする為に事前の記入をお願いしております。',
         type: 'website',
-        url: 'https://olivebodycare.healthcare/about/medical-questionnaire.vue',
+        url: 'https://olivebodycare.healthcare/about/medical-questionnaire',
         image: 'https://olivebodycare.healthcare/assets/images/hed_1.jpg'
       },
       itemsB: [
