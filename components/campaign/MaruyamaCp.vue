c89180a15a7c114a6fdd42dbcf5232e4b7467574
gakumac <gaku.machida@gmail.com>
2020-07-01 12:06:41 +0900

    キャンペーン情報の訂正

--- components/campaign/MaruyamaCp.vue
+++ components/campaign/MaruyamaCp.vue
@@ -1,27 +1,11 @@
 <template>
-  <div>
-    <div class="info">
-      <v-layout justify-center>
-        <v-flex xs12 sm11 md9 lg8 xl7>
-          <v-img
-            :src="require('@/assets/images/campaign/maruyama/maruyama-cp8.jpg')"
-          ></v-img>
-          <p class="caption">
-            ※ご新規さま限定で通常施術の1時間分が半額になります。通常通りご予約お願いします。予約時に『特典希望』とコメント記載またはお伝えください。
-          </p>
-        </v-flex>
-      </v-layout>
-    </div>
-    <div class="info mt-3">
-      <v-layout justify-center>
-        <v-flex xs12 sm11 md9 lg8 xl7>
-          <v-img
-            :src="
-              require('@/assets/images/campaign/maruyama/maruyama-cp1.2.jpg')
-            "
-          ></v-img>
-        </v-flex>
-      </v-layout>
-    </div>
+  <div class="info">
+    <v-layout justify-center>
+      <v-flex xs12 sm11 md9 lg8 xl7>
+        <v-img
+          :src="require('@/assets/images/campaign/maruyama/maruyama-cp1.2.jpg')"
+        ></v-img>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
