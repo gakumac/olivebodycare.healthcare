c1880e81bc3ca1f64696250911dbcb74a8e78558
gakumac <gaku.machida@gmail.com>
2020-08-06 11:02:31 +0900

    円山店キャンペーン情報の追加

--- components/campaign/MaruyamaCp.vue
+++ components/campaign/MaruyamaCp.vue
@@ -3,6 +3,16 @@
     <v-layout justify-center>
       <v-flex xs12 sm11 md9 lg8 xl7>
         <v-img
+          :src="require('@/assets/images/campaign/maruyama/maruyama-cp9.png')"
+        ></v-img>
+        <p class="caption">
+          ※ご予約時ご要望・ご相談欄に『特典希望』とご記入頂くか、スタッフに直接お伝えください。
+        </p>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-center class="mt-1">
+      <v-flex xs12 sm11 md9 lg8 xl7>
+        <v-img
           :src="require('@/assets/images/campaign/maruyama/maruyama-cp1.2.jpg')"
         ></v-img>
       </v-flex>
